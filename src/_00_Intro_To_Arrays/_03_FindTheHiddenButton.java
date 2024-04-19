415069fee1ce84ab6d2a0a100f42b14d4b94d4cc
league <league@192.168.1.85>
2024-04-19 16:59:37 -0700

    ggg

--- src/_00_Intro_To_Arrays/_03_FindTheHiddenButton.java
+++ src/_00_Intro_To_Arrays/_03_FindTheHiddenButton.java
@@ -14,7 +14,8 @@ import javax.swing.JPanel;
 public class _03_FindTheHiddenButton implements ActionListener{
     JFrame window;
     JPanel panel;
-
+    int hiddenVar ;
+    JButton[] button ;
     // 1. create an array of JButtons. Don't initialize it yet.
 
     // 2. create an integer variable called hiddenButton
@@ -24,8 +25,10 @@ public class _03_FindTheHiddenButton implements ActionListener{
     }
 
     public void start() {
+    	
         window = new JFrame("Find the Button");
         panel = new JPanel();
+    
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         // 3. Ask the user to enter a positive number and convert it to an int
@@ -33,31 +36,46 @@ public class _03_FindTheHiddenButton implements ActionListener{
         // 4. Initialize the array of JButtons to be the size of the int
         //    created in step 3
 int inti = Integer.parseInt(ing);
+ button = new JButton[inti];
+
         // 5. Make a for loop to iterate through the JButton array
-        
+        for( int i = 0; i < button.length ; i++ ) {
+        button[i] = new JButton();
+        panel.add(button[i]);
+        button[i].addActionListener(this);
+        button[i].setVisible(true);
+        }
             // 6. initialize each JButton in the array
-        
+    
             // 7. add the ActionListener to each JButton
         
             // 8. add each JButton to the panel
 
         // 9 add the panel to the window
-
+        window.add(panel);
+        
         // 10. call setExtendedState(JFrame.MAXIMIZED_BOTH) on your JFrame object.
-
+window.setExtendedState(JFrame.MAXIMIZED_BOTH);
         // 11. set the JFrame to visible.
-
+window.setVisible(true);
         // 12. Give the user the instructions for the game.
-
+JOptionPane.showMessageDialog(null, "Rules:"
+		+ "1) find the me button, 2) when the label disapears click the button, 3) you click the correct button you win.");
         // 13. initialize the hiddenButton variable to a random number less than
         //     the int created in step 3
-
+Random r = new Random();
+        hiddenVar = r.nextInt(inti);
         // 14. Set the text of the JButton located at hiddenButton to read "ME"
-
+        	button[hiddenVar].setText("THE, CHOOSE THE, METHINKS YE HAVE TRUST IT IT THE.");
         // 15. Use Thread.sleep(100); to pause the program.
-
+        	try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
         // 16. Set the text of the JButton located at hiddenButton to be blank.
-
+        	button[hiddenVar].setText("");
     }
 
     @Override
@@ -65,7 +83,11 @@ int inti = Integer.parseInt(ing);
         JButton buttonClicked = (JButton)e.getSource();
 
         // 17. if the hiddenButton is clicked, tell the user that they win.
-
+if( buttonClicked == button[hiddenVar]) {
+	JOptionPane.showMessageDialog(null, "you may win this time but I'll be back, especially for Mary.");
+} else {
+	JOptionPane.showMessageDialog(null, "well, well, well, this town was never big enough for the both of us afterall was it? Muhahahahahahhasdahaha Leave this town loser, Mary is my giurlfriend now...");
         // 18. else tell them to try again
     }
+}
 }
--- src/_01_Simple_Array_Algorithms/MoreArrayFun.java
+++ src/_01_Simple_Array_Algorithms/MoreArrayFun.java
@@ -1,18 +1,27 @@
 package _01_Simple_Array_Algorithms;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import java.util.Random;
 
 public class MoreArrayFun {
     //1. Create a main method to test the other methods you write.
 	public static void main(String[] args) {
+		MoreArrayFun fyup = new MoreArrayFun();
+		  String[] arr = {"bianca", "orange", "red", "green"};
+	         fyup.strarray(arr);
+	         fyup.strarray2(arr);
+	         fyup.strarray3(arr);
+	         fyup.strarray4(arr);
+
 		
 	}
 
 
     //2. Write a method that takes an array of Strings and prints all the Strings in the array.
  void strarray( String[] arr) {
-	for( int i = 0; i < arr.length-1; i++) {
-		System.out.println(i);
+	for( int i = 0; i < arr.length; i++) {
+		System.out.println(arr[i]);
 	}
 }
 
@@ -21,7 +30,7 @@ public class MoreArrayFun {
     //   in reverse order.
  void strarray2( String[] arr) {
 		for( int i =  arr.length-1; i > -1; i--) {
-			System.out.println(i);
+			System.out.println(arr[i]);
 		}
 	}
 
@@ -31,7 +40,7 @@ public class MoreArrayFun {
 	 boolean toprint = true;
 		for( int i =  arr.length-1; i > -1; i--) {
 			if( toprint == true) {
-			System.out.println(i);
+			System.out.println(arr[i]);
 			toprint = false;
 			}else {
 				toprint = true;
@@ -42,6 +51,23 @@ public class MoreArrayFun {
 
     //5. Write a method that takes an array of Strings and prints all the Strings in the array
     //   in a completely random order. Almost every run of the program should result in a different order.
+ void strarray4( String[] arr) {
+	 Random r = new Random();
+	 boolean toprint = false;
+		for( int i =  arr.length-1; i > -1; i--) {
+			 int rat = r.nextInt(2);
+			 if( rat == 0) {
+			 toprint = true;
+			 } else {
+				 toprint = false;
+			 }
+			if( toprint == true) {
+			System.out.println(arr[i]);
+			
+			}
+
+		}
+	}
 
 
 }
